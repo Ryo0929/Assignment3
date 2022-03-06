d6afc9bae3da044a63654c6fc1bebaf63557fa95
Weitung Liao <>
2022-03-06 12:12:56 -0700

    feature: writing result to txt

--- src/main/java/ntpClient.java
+++ src/main/java/ntpClient.java
@@ -1,7 +1,7 @@
 import org.apache.commons.net.ntp.TimeInfo;
 import org.apache.commons.net.ntp.TimeStamp;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -9,26 +9,46 @@ import java.util.List;
 
 public class ntpClient {
     public static void main(String args[]) throws IOException, InterruptedException {
+        File fout = new File("offset_and_delay.txt");
+        FileOutputStream fos = new FileOutputStream(fout);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 
-        //while(true){
-        //    Thread.sleep(10 * 1000);
+        File fout2 = new File("raw.txt");
+        FileOutputStream fos2 = new FileOutputStream(fout2);
+        BufferedWriter raw = new BufferedWriter(new OutputStreamWriter(fos2));
+        raw.write("<burst #, pair #> T1, T2, T3, T4");
+        raw.newLine();
+
+        File fout3 = new File("update_values.txt");
+        FileOutputStream fos3 = new FileOutputStream(fout3);
+        BufferedWriter bw2 = new BufferedWriter(new OutputStreamWriter(fos3));
+
+        int count=0;
+        while(count<15){
+
             ntpClientImpl client = new ntpClientImpl();
             client.open();
             List<Long> delayValues = new ArrayList<Long>();
             List<Long> offsetValues = new ArrayList<Long>();
 
-
             for(int i=0;i<8;i+=1){
-//                InetAddress hostAddr = InetAddress.getByName("tick.mit.edu");
-                InetAddress hostAddr = InetAddress.getLocalHost();
 
-                TimeInfo info = client.getTime(hostAddr,1023);
-                info.computeDetails(); // compute offset/delay if not already done
+                //Public server
+                    //InetAddress hostAddr = InetAddress.getByName("tick.mit.edu");
+                    //TimeInfo info = client.getTime(hostAddr,123);
 
+                //Cloud server
+                    byte[] ipAddr = new byte[] { 34, 125, 117, 102 };
+                    InetAddress hostAddr = InetAddress.getByAddress(ipAddr);
+                    TimeInfo info = client.getTime(hostAddr,1023);
+
+                info.computeDetails();
                 TimeStamp t1=info.getMessage().getOriginateTimeStamp();
                 TimeStamp t2=info.getMessage().getReceiveTimeStamp();
                 TimeStamp t3=info.getMessage().getTransmitTimeStamp();
                 TimeStamp t4=info.getMessage().getReferenceTimeStamp();
+                raw.write("<burst "+count+", pair "+i+"> "+t1.toString()+", "+t2.toString()+", "+t3.toString()+", "+t4.toString());
+                raw.newLine();
 
                 Long offsetValue = info.getOffset();
                 Long delayValue = info.getDelay();
@@ -38,8 +58,9 @@ public class ntpClient {
                 delayValues.add(delayValue);
                 offsetValues.add(offsetValue);
 
-                System.out.println(" Roundtrip delay(ms)=" + delay
-                        + ", clock offset(ms)=" + offset); // offset in ms
+                System.out.println(" Roundtrip delay(ms)=" + delay + ", clock offset(ms)=" + offset); // offset in ms
+                bw.write("<"+count+"/"+i+">, "+delay+", "+offset);
+                bw.newLine();
             }
 
             Long updateValueTheta= Collections.min(delayValues);
@@ -48,8 +69,15 @@ public class ntpClient {
             String theta = (updateValueTheta == null) ? "N/A" : updateValueTheta.toString();
             String delta = (updateValueDelta == null) ? "N/A" : updateValueDelta.toString();
             System.out.println(" Update values ("+theta+", "+delta+")");
+            bw2.write("<"+count+">"+", "+theta+", "+delta);
 
             client.close();
-        //}
+            count+=1;
+            System.out.println("sleep...");
+            Thread.sleep(120 * 1000);
+        }
+
+        bw.close();
+        raw.close();
     }
 }
